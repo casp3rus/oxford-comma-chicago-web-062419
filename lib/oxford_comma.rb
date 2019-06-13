1cef06285e18ca0a82479ce675f8122988bc563e
Adrian Popan <adrian.popan@gmail.com>
2019-06-13 00:56:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
-  array.join
   if array.length == 2 
     puts "#{array[0]} and #{array[1]}"
+  else 2 < array.length
+    array[-1].insert(0, "and ")
   end
+  array.join(", ")
 end
